154be886d0ac79e435e20d0b2a99072beb6f9037
andreea <andreea.nec10@gmail.com>
2020-10-02 02:13:53 +0300

    Add 2 methods of counting elements of a dict and process_data

--- Examples of scripts/test_csv.py
+++ Examples of scripts/test_csv.py
@@ -11,5 +11,28 @@ def read_employees(path):
             employee_list.append(data)
         return employee_list
 
+def make_dict_of_frequency_I(list):
+    dict = {}
+    for elem in list:
+        if elem not in dict:
+            dict[elem] = 0
+        dict[elem] += 1
+    return dict
+
+def make_dict_of_frequency_II(list):
+    dict = {}
+    for elem in set(list):
+        dict[elem] = list.count(elem)
+    return dict
+
+def process_data(employee_list):
+    department_list = []
+    for employee in employee_list:
+        department_list.append(employee["department"])
+    department_data = make_dict_of_frequency_II(department_list)
+    return department_data
+
+
 employee_list = read_employees("./by_department.csv")
-print(employee_list)
+dictionary = process_data(employee_list)
+print(dictionary)
